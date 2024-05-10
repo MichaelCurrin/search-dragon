06a27f76b5de26ce12b38921125c2c4ba93f10a2
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-05-10 16:57:16 +0200

    feat: add Google Analytics tracking

---
+++ assets/js/analytics.js
@@ -0,0 +1,10 @@
+const TRACK_DOMAINS = ['michaelcurrin.github.io', 'search-dragon.com'];
+const track = TRACK_DOMAINS.includes(window.location.hostname);
+
+if (track) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-MBQMD6HN83');
+}
