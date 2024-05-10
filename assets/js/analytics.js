f506d873f4ea9073752e9965b95aee9b98481efb
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-05-10 17:01:58 +0200

    style: format

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,10 +1,12 @@
-const TRACK_DOMAINS = ['michaelcurrin.github.io', 'search-dragon.com'];
+const TRACK_DOMAINS = ["michaelcurrin.github.io", "search-dragon.com"];
 const track = TRACK_DOMAINS.includes(window.location.hostname);
 
 if (track) {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
 
-    gtag('config', 'G-MBQMD6HN83');
+  gtag("config", "G-MBQMD6HN83");
 }
